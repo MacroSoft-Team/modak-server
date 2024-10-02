104b60f9db3e9fc2d26d829be157aa8de5faae50
dgh06175 <dgh06175@gmail.com>
2024-10-02 12:04:40 +0900

    init project

---
+++ src/main/java/com/macrosoft/modakserver/ModakServerApplication.java
@@ -0,0 +1,13 @@
+package com.macrosoft.modakserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModakServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModakServerApplication.class, args);
+    }
+
+}
