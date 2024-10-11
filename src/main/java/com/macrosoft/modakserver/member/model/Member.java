8a64b77568da2400c9f23f08c6d6d2a89c8f7409
dgh06175 <dgh06175@gmail.com>
2024-10-11 16:09:01 +0900

    feat: #5 멤버 클래스 작성

--- src/main/java/com/macrosoft/modakserver/member/model/Member.java
+++ src/main/java/com/macrosoft/modakserver/member/model/Member.java
@@ -1,4 +1,27 @@
 package com.macrosoft.modakserver.member.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Builder
+@Entity
+@Table(name = "member")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    @Column(name = "social_id")
+    private String socialId;
+
+//    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+//    private List<Log> logs; // 1:N 관계
 }
