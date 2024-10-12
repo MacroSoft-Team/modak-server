238bc8b6f4e499ab20fcf92623f95f59df331f17
dgh06175 <dgh06175@gmail.com>
2024-10-12 21:34:35 +0900

    chore: #5 자동 배포 jar 이름 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,10 +10,14 @@ START_LOG="$ROOT_PATH/start.log"
 NOW=$(date +%c)
 
 echo "[$NOW] $JAR 복사" >> $START_LOG
-cp $ROOT_PATH/build/libs/modak-server-1.0.0.jar $JAR
+cp $ROOT_PATH/build/libs/modak-server-0.0.1-SNAPSHOT.jar $JAR
 
 echo "[$NOW] > $JAR 실행" >> $START_LOG
 nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
 
 SERVICE_PID=$(pgrep -f $JAR)
 echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
+
+[Sat Oct 12 12:29:58 2024] /home/ubuntu/modak-server/application.jar 복사
+[Sat Oct 12 12:29:58 2024] > /home/ubuntu/modak-server/application.jar 실행
+[Sat Oct 12 12:29:58 2024] > 서비스 PID:
