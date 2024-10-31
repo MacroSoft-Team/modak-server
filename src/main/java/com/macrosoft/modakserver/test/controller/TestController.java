73de7934b4ea5af1d9592159d28df9d6cca6768e
dgh06175 <dgh06175@gmail.com>
2024-10-31 14:38:27 +0900

    feat: 테스트 API, 로그 API 문서에서 비활성화 처리

--- src/main/java/com/macrosoft/modakserver/test/controller/TestController.java
+++ src/main/java/com/macrosoft/modakserver/test/controller/TestController.java
@@ -3,6 +3,7 @@ package com.macrosoft.modakserver.test.controller;
 import com.macrosoft.modakserver.domain.member.entity.Member;
 import com.macrosoft.modakserver.global.BaseResponse;
 import com.macrosoft.modakserver.test.service.TestService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Test API")
 public class TestController {
     private final TestService testService;
-    
+
+    @Operation(deprecated = true)
     @GetMapping("/get")
     public BaseResponse<List<Member>> get() {
         List<Member> memberList = testService.get();
